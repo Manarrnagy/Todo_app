79836a1416eab7572bc44db71f9de10b100906ea
manarrnagy <manarrnagy@gmail.com>
2023-01-29 01:32:34 +0200

    first commit

---
+++ app/src/main/java/com/route/todoapp/home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.route.todoapp.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.route.todoapp.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
---
+++ app/src/main/java/com/route/todoapp/ui/SplashActivity.kt
@@ -0,0 +1,29 @@
+package com.route.todoapp.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.route.todoapp.R
+import com.route.todoapp.home.HomeActivity
+
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        Handler().postDelayed(object: Runnable{
+            override fun run() {
+                startHomeScreen()
+            }
+
+        },2000)
+
+    }
+
+    private fun startHomeScreen() {
+        val intent = Intent(this,HomeActivity::class.java )
+        startActivity(intent)
+    }
+
+}
